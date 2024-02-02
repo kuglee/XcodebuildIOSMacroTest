20d92bde33edc8dedc5c48028ea6d7a9d9a8592a
Gábor Librecz <kuglee@gmail.com>
2024-02-02 13:31:18 +0100

    Initial Commit

---
+++ XcodebuildIOSMacroTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XcodebuildIOSMacroTest
+//
+//  Created by Librecz Gábor on 02/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XcodebuildIOSMacroTest/XcodebuildIOSMacroTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodebuildIOSMacroTestApp.swift
+//  XcodebuildIOSMacroTest
+//
+//  Created by Librecz Gábor on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct XcodebuildIOSMacroTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
